cf16b50e9e1efdd8e447b31ff365574971f2d5c1
jessehogan0 <jessehogan0@gmail.com>
2015-05-12 08:57:41 +0000

    Add reversed() call

--- data-structures.py
+++ data-structures.py
@@ -2,7 +2,7 @@ lol = [[1, 2, 3],[4, 5, 6], [7, 8, 9]]
 
 def sqr(x): return pow(x,2)
 
-lol = [map(sqr, ls) for ls in lol]
+lol = [map(sqr, ls) for ls in reversed(lol)]
 print(lol)
 
 lol = [e for ls in lol for e in ls if e > 25]
