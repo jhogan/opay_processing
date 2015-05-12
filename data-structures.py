d66fddd18dc221b3e46b1c1072a87776ea15003e
jessehogan0 <jessehogan0@gmail.com>
2015-05-12 09:05:52 +0000

    Removed a print line.

--- data-structures.py
+++ data-structures.py
@@ -3,5 +3,4 @@ lol = [[1, 2, 3],[4, 5, 6], [7, 8, 9]]
 lol = [map(lambda x: pow(x, 2), ls) for ls in reversed(lol)]
 print(lol)
 
-lol = [e for ls in lol for e in ls if e > 25]
-print(lol)
+print([e for ls in lol for e in ls if e > 25])
