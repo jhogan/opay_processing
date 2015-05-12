f8760b699ac182e9ab8b35fb7398f4b0a0390335
jessehogan0 <jessehogan0@gmail.com>
2015-05-12 09:00:58 +0000

    Use lambda.

--- data-structures.py
+++ data-structures.py
@@ -1,8 +1,6 @@
 lol = [[1, 2, 3],[4, 5, 6], [7, 8, 9]]
 
-def sqr(x): return pow(x,2)
-
-lol = [map(sqr, ls) for ls in reversed(lol)]
+lol = [map(lambda x: pow(x, 2), ls) for ls in reversed(lol)]
 print(lol)
 
 lol = [e for ls in lol for e in ls if e > 25]
