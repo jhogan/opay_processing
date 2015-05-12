0090ca7178f9a6c0426861731e49d28a609045a5
jessehogan0 <jessehogan0@gmail.com>
2015-05-12 07:50:32 +0000

    Write gevent sample.

---
+++ gevent-sample.py
@@ -0,0 +1,11 @@
+import gevent
+from random import randint
+
+def job():
+    rnd = randint(1, 10000) / float(1000)
+    gevent.sleep(rnd);
+    return rnd
+
+jobs = [gevent.spawn(job) for i in range(3)];
+gevent.joinall(jobs)
+print [job.value for job in jobs]
